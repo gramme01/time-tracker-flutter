920c92d7817ac2f2bc930a224b61dd95d55a05ae
Emmanuel Oguntoyinbo <gramme.oe@gmail.com>
2020-06-02 21:38:32 +0100

    Completes login UI

--- lib/app/sign_in/sign_in_page.dart
+++ lib/app/sign_in/sign_in_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'sign_in_button.dart';
+import 'social_sign_in_button.dart';
+
 class SignInPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -9,6 +12,7 @@ class SignInPage extends StatelessWidget {
         elevation: 2.0,
       ),
       body: _buildContent(),
+      backgroundColor: Colors.grey[200],
     );
   }
 
@@ -27,22 +31,48 @@ class SignInPage extends StatelessWidget {
               fontWeight: FontWeight.w600,
             ),
           ),
-          SizedBox(height: 8.0),
-          Container(
-            color: Colors.red,
-            child: SizedBox(
-              height: 100,
-            ),
+          SizedBox(height: 48.0),
+          SocialSignInButton(
+            text: 'Sign in with Google',
+            assetName: 'images/google-logo.png',
+            textColor: Colors.black87,
+            color: Colors.white,
+            onPressed: _signInWithGoogle,
           ),
           SizedBox(height: 8.0),
-          Container(
-            color: Colors.purple,
-            child: SizedBox(
-              height: 100,
-            ),
+          SocialSignInButton(
+            text: 'Sign in with Facebook',
+            assetName: 'images/facebook-logo.png',
+            textColor: Colors.white,
+            color: Color(0xFF334D92),
+            onPressed: () {},
+          ),
+          SizedBox(height: 8.0),
+          SignInButton(
+            text: 'Sign in with Email',
+            textColor: Colors.white,
+            color: Colors.teal[700],
+            onPressed: () {},
+          ),
+          SizedBox(height: 8.0),
+          Text(
+            'or',
+            style: TextStyle(fontSize: 14.0, color: Colors.black87),
+            textAlign: TextAlign.center,
+          ),
+          SizedBox(height: 8.0),
+          SignInButton(
+            text: 'Go anonymous',
+            textColor: Colors.black,
+            color: Colors.lime[300],
+            onPressed: () {},
           ),
         ],
       ),
     );
+  }
+
+  void _signInWithGoogle() {
+    // TODO: Auth with Google
   }
 }
