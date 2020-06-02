0f033d901ae5ba4ea78771647d8782fdd4138c01
Emmanuel Oguntoyinbo <gramme.oe@gmail.com>
2020-06-02 18:59:52 +0100

    Sets up Login Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:time_tracker/app/sign_in/sign_in_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -7,9 +8,12 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'Time Tracker',
-    theme: ThemeData(
-      primarySwatch: Colors.indigo,
-    ),);
+    return MaterialApp(
+      title: 'Time Tracker',
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+      ),
+      home: SignInPage(),
+    );
   }
 }
