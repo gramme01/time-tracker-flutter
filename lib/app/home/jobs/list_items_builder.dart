5dad99982fafba2f3b668dfaee48a13056963b13
Emmanuel Oguntoyinbo <gramme.oe@gmail.com>
2020-07-02 22:10:18 +0100

    Generic buider

--- lib/app/home/jobs/list_items_builder.dart
+++ lib/app/home/jobs/list_items_builder.dart
@@ -1,13 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:time_tracker/app/home/jobs/empty_content.dart';
 
 typedef ItemWidgetBuilder<T> = Widget Function(BuildContext context, T item);
 
-
-class ListItemsBUilder extends StatelessWidget {
+class ListItemsBuilder<T> extends StatelessWidget {
+  final AsyncSnapshot<List<T>> snapshot;
+  final ItemWidgetBuilder<T> itemBuilder;
+  const ListItemsBuilder({
+    Key key,
+    this.snapshot,
+    this.itemBuilder,
+  }) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
-    );
+    if (snapshot.hasData) {
+      final List<T> items = snapshot.data;
+      if (items.isNotEmpty) {
+        // TODO: return ListVIew
+      } else {
+        return EmptyContent();
+      }
+    }
+    return Container();
   }
 }
