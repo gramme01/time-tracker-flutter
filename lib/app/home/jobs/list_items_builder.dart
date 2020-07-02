1acb222f47d2a274db237a1dd5b91a8c8bd4200d
Emmanuel Oguntoyinbo <gramme.oe@gmail.com>
2020-07-02 22:04:42 +0100

    Start 02-07

---
+++ lib/app/home/jobs/list_items_builder.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+typedef ItemWidgetBuilder<T> = Widget Function(BuildContext context, T item);
+
+
+class ListItemsBUilder extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
