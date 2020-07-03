e676cb70c96230b4e8966a33213d2e29b42a63ba
Emmanuel Oguntoyinbo <gramme.oe@gmail.com>
2020-07-03 06:26:35 +0100

    Builds List view with separator

--- lib/app/home/jobs/jobs_page.dart
+++ lib/app/home/jobs/jobs_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:time_tracker/app/home/jobs/empty_content.dart';
 
 import '../../../common_widgets/platform_alert_dialog.dart';
 import '../../../services/auth.dart';
@@ -8,6 +7,7 @@ import '../../../services/database.dart';
 import '../models/job.dart';
 import 'edit_job_page.dart';
 import 'job_list_tile.dart';
+import 'list_items_builder.dart';
 
 class JobsPage extends StatelessWidget {
   Future<void> _signout(BuildContext context) async {
@@ -58,29 +58,12 @@ class JobsPage extends StatelessWidget {
     return StreamBuilder<List<Job>>(
       stream: database.jobsStream(),
       builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          final jobs = snapshot.data;
-          if (jobs.isNotEmpty) {
-            final children = jobs
-                .map((job) => JobListTile(
-                      job: job,
-                      onTap: () => EditJobPage.show(context, job: job),
-                    ))
-                .toList();
-            return ListView(
-              children: children,
-            );
-          } else {
-            return EmptyContent();
-          }
-        }
-        if (snapshot.hasError) {
-          return Center(
-            child: Text('Unable to load job feed'),
-          );
-        }
-        return Center(
-          child: CircularProgressIndicator(),
+        return ListItemsBuilder<Job>(
+          snapshot: snapshot,
+          itemBuilder: (context, job) => JobListTile(
+            job: job,
+            onTap: () => EditJobPage.show(context, job: job),
+          ),
         );
       },
     );
--- lib/app/home/jobs/list_items_builder.dart
+++ lib/app/home/jobs/list_items_builder.dart
@@ -8,19 +8,39 @@ class ListItemsBuilder<T> extends StatelessWidget {
   final ItemWidgetBuilder<T> itemBuilder;
   const ListItemsBuilder({
     Key key,
-    this.snapshot,
-    this.itemBuilder,
+    @required this.snapshot,
+    @required this.itemBuilder,
   }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     if (snapshot.hasData) {
       final List<T> items = snapshot.data;
       if (items.isNotEmpty) {
-        // TODO: return ListVIew
+        return _buildList(items);
       } else {
         return EmptyContent();
       }
+    } else if (snapshot.hasError) {
+      return EmptyContent(
+        title: 'Something went wrong',
+        message: 'Can\'t load items right now',
+      );
     }
-    return Container();
+    return Center(child: CircularProgressIndicator());
+  }
+
+  Widget _buildList(List<T> items) {
+    return ListView.separated(
+        itemCount: items.length + 2,
+        separatorBuilder: (context, index) => Divider(
+              height: 0.5,
+            ),
+        itemBuilder: (context, index) {
+          if (index == 0 || index == items.length + 1) {
+            return Container();
+          }
+          return itemBuilder(context, items[index - 1]);
+        });
   }
 }
