ebf4cc087865c5ce7affc4b4c7bf295b1904035a
Emmanuel Oguntoyinbo <gramme.oe@gmail.com>
2020-07-03 06:34:01 +0100

    Adds Delete job form firebase functionality

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -7,6 +7,7 @@ import 'firestore_service.dart';
 abstract class Database {
   // Future<void> setJob(Job job);
   Future<void> setJob(Job job);
+  Future<void> deleteJob(Job job);
   Stream<List<Job>> jobsStream();
 }
 
@@ -17,11 +18,16 @@ class FirestoreDatabase implements Database {
   FirestoreDatabase({@required this.uid}) : assert(uid != null);
 
   final _service = FirestoreService.instance;
+
   @override
   Future<void> setJob(Job job) async => await _service.setData(
         path: APIPath.job(uid, job.id),
         data: job.toMap(),
       );
+
+  @override
+  Future<void> deleteJob(Job job) async =>
+      await _service.deleteData(path: APIPath.job(uid, job.id));
 
   @override
   Stream<List<Job>> jobsStream() => _service.collectionStream(
