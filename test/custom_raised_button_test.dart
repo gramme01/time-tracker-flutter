b760ceaf29dd7040d0a8e432b5ec149c7ed4fb07
Emmanuel Oguntoyinbo <gramme.oe@gmail.com>
2020-07-17 19:50:22 +0100

    [TEST] Finds widget and matcher argument

---
+++ test/custom_raised_button_test.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:time_tracker/common_widgets/custom_raised_button.dart';
+
+void main() {
+  testWidgets('', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      MaterialApp(
+        home: CustomRaisedButton(
+          child: Text('tap me'),
+        ),
+      ),
+    );
+    final button = find.byType(RaisedButton);
+    expect(button, findsOneWidget);
+    expect(find.byType(FlatButton), findsNothing);
+    expect(find.text('tap me'), findsOneWidget);
+  });
+}
