f2f4e7f10c101607178f2f2cfd0fac95c2b7bd19
Emmanuel Oguntoyinbo <gramme.oe@gmail.com>
2020-07-19 22:46:59 +0100

    [TEST] Stub Stream for StreamBuilders

--- test/landing_page_test.dart
+++ test/landing_page_test.dart
@@ -1,18 +1,28 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 import 'package:provider/provider.dart';
+import 'package:time_tracker/app/home/home_page.dart';
 
 import 'package:time_tracker/app/landing_page.dart';
+import 'package:time_tracker/app/sign_in/sign_in_page.dart';
 import 'package:time_tracker/services/auth.dart';
 
 class MockAuth extends Mock implements AuthBase {}
 
 void main() {
   MockAuth mockAuth;
+  StreamController<User> onAuthStateChangedController;
 
   setUp(() {
     mockAuth = MockAuth();
+    onAuthStateChangedController = StreamController<User>();
+  });
+
+  tearDown(() {
+    onAuthStateChangedController.close();
   });
 
   Future<void> pumpLangingPage(WidgetTester tester) async {
@@ -24,10 +34,34 @@ void main() {
         ),
       ),
     );
+    await tester.pump();
   }
 
   void stubOnAuthStateChangeYields(Iterable<User> onAuthStateChanged) {
+    onAuthStateChangedController
+        .addStream(Stream<User>.fromIterable(onAuthStateChanged));
     when(mockAuth.onAuthStateChanged)
-        .thenAnswer((_) => Stream<User>.fromIterable(onAuthStateChanged));
+        .thenAnswer((_) => onAuthStateChangedController.stream);
   }
+
+  testWidgets('Stream Waiting', (WidgetTester tester) async {
+    stubOnAuthStateChangeYields([]);
+
+    await pumpLangingPage(tester);
+    expect(find.byType(CircularProgressIndicator), findsOneWidget);
+  });
+
+  testWidgets('Null User', (WidgetTester tester) async {
+    stubOnAuthStateChangeYields([null]);
+
+    await pumpLangingPage(tester);
+    expect(find.byType(SignInPage), findsOneWidget);
+  });
+
+  testWidgets('Non-null User', (WidgetTester tester) async {
+    stubOnAuthStateChangeYields([User(uid: 'abc')]);
+
+    await pumpLangingPage(tester);
+    expect(find.byType(Homepage), findsOneWidget);
+  });
 }
