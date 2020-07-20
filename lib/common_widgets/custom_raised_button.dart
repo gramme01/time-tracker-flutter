82c7df9c6c211b736ed03cee5665d4c31351cf7a
Emmanuel Oguntoyinbo <gramme.oe@gmail.com>
2020-07-20 22:16:27 +0100

    [TEST] Adds key parameter

--- lib/common_widgets/custom_raised_button.dart
+++ lib/common_widgets/custom_raised_button.dart
@@ -1,19 +1,22 @@
 import 'package:flutter/material.dart';
 
 class CustomRaisedButton extends StatelessWidget {
+
   final Widget child;
   final Color color;
   final double borderRadius;
   final double height;
   final VoidCallback onPressed;
 
-  const CustomRaisedButton({
+  CustomRaisedButton({
+    Key key,
     this.child,
     this.color,
     this.borderRadius: 2.0,
     this.height: 50.0,
     this.onPressed,
-  }) : assert(borderRadius != null);
+  })  : assert(borderRadius != null),
+        super(key: key);
 
   @override
   Widget build(BuildContext context) {
