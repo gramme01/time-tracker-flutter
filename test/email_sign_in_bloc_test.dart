6b0707e1d6ba7ed19652254c576eb1c8b0f00b60
Emmanuel Oguntoyinbo <gramme.oe@gmail.com>
2020-07-21 15:59:00 +0100

    [TEST] TEsts model blocs
    write expectations upfront with emitsInOrder

--- test/email_sign_in_bloc_test.dart
+++ test/email_sign_in_bloc_test.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
 import 'package:time_tracker/app/sign_in/email_sign_in_bloc.dart';
 import 'package:time_tracker/app/sign_in/email_sign_in_model.dart';
 
@@ -18,10 +20,45 @@ void main() {
   });
 
   test(
-      'WHEN email is updated '
-      'AND password is updated '
-      'AND submit is called '
-      'THEN modelStream emits the correct events', () async {
-    expect(bloc.modelStream, emits(EmailSignInModel()));
-  });
+    'WHEN email is updated '
+    'AND password is updated '
+    'AND submit is called '
+    'THEN modelStream emits the correct events',
+    () async {
+      when(mockAuth.signInWithEmailAndPassword(any, any)).thenThrow(
+          PlatformException(code: 'ERROR', message: 'Error Signing In'));
+      String sampleEmail = 'email@email.com';
+      String samplePassword = '123456';
+
+      expect(
+        bloc.modelStream,
+        emitsInOrder([
+          EmailSignInModel(),
+          EmailSignInModel(email: sampleEmail),
+          EmailSignInModel(
+            email: sampleEmail,
+            password: samplePassword,
+          ),
+          EmailSignInModel(
+            email: sampleEmail,
+            password: samplePassword,
+            hasSubmitted: true,
+            isLoading: true,
+          ),
+          EmailSignInModel(
+            email: sampleEmail,
+            password: samplePassword,
+            hasSubmitted: true,
+            isLoading: false,
+          ),
+        ]),
+      );
+
+      bloc.updateEmail(sampleEmail);
+      bloc.updatePswrd(samplePassword);
+      try {
+        await bloc.submit();
+      } catch (e) {}
+    },
+  );
 }
